fbc5e73561e149c0c0c9b8353441e135bf63d59a
thelostsoul <1095533751@qq.com>
2018-09-11 11:32:59 +0800

    1st

---
+++ LevelHighlightingCompositeConverter.java
@@ -0,0 +1,29 @@
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ANSIConstants;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+/**
+ * 日志级别高亮
+ */
+public class LevelHighlightingCompositeConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase#
+     * getForegroundColorCode(java.lang.Object)
+     */
+    protected String getForegroundColorCode(ILoggingEvent event) {
+        switch (event.getLevel().toInt()) {
+            case Level.ERROR_INT:
+                return ANSIConstants.BOLD+ANSIConstants.RED_FG;
+            case Level.INFO_INT:
+                return ANSIConstants.BOLD+ANSIConstants.YELLOW_FG;
+            case Level.DEBUG_INT:
+                return ANSIConstants.BOLD+ANSIConstants.GREEN_FG;
+            default:
+                return null;
+        }
+    }
+}
---
+++ ProcessIdClassicConverter.java
@@ -0,0 +1,26 @@
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+/**
+ * 进程ID 获取
+ *
+ * @author kun
+ *
+ */
+public class ProcessIdClassicConverter extends ClassicConverter {
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see ch.qos.logback.core.pattern.Converter#convert(java.lang.Object)
+     */
+    public String convert(ILoggingEvent event) {
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        String name = runtime.getName();
+        return name.substring(0, name.indexOf("@"));
+    }
+
+}
